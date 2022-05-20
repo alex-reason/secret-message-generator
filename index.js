176973a4e0562d6a773ce6a07d720add01eada3f
Mai <90660443+alex-reason@users.noreply.github.com>
2022-05-20 14:23:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const {hash} = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('#message-header').innerText = message;
+}
+
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+
+    const input = document.querySelector('#message-input');
+    //to convert to Base 64: btoa(); to convert back: atob();
+    const encrypted = btoa(input.value);
+    
+    document.querySelector('#encrypted-message').innerText = encrypted;
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select(); 
+
+});
